e77d403b993fdeab17a18fd0a7b1853b7e16bdc8
Norris Luong <33255034+Gsirron@users.noreply.github.com>
2024-04-16 19:18:18 +1000

    Feat: Added CheckIfSettlementsAreFullAsync

--- BookingApi/Services/BookingService.cs
+++ BookingApi/Services/BookingService.cs
@@ -3,6 +3,7 @@ using BookingApi.Exceptions;
 using BookingApi.Interfaces;
 using BookingApi.Models.Dto;
 using BookingApi.Models.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookingApi.Services
 {
@@ -22,6 +23,7 @@ namespace BookingApi.Services
                 throw new InvalidBookingDetailsException("Name or BookingTime cannot be empty");
             }
 
+            // Validate BookingTime is a in a Valid Format
             var bookingTime = new TimeOnly();
             var ParseResult = TimeOnly.TryParse(newBooking.BookingTime, out bookingTime);
 
@@ -30,6 +32,7 @@ namespace BookingApi.Services
                 throw new InvalidBookingDetailsException("BookingTime must be in the format HH:mm");
             }
 
+            // Checks if the BookingTime is Between 09:00 to 16:00
             if (!bookingTime.IsBetween(new TimeOnly(8, 59), new TimeOnly(16, 01)))
             {
                 throw new InvalidBookingDetailsException("BookingTime must be between 09:00 to 16:00 hours");
@@ -38,5 +41,20 @@ namespace BookingApi.Services
             return null;
 
         }
+        /// <summary>
+        /// Checks the Database for the number of bookings during a particular Timeframe
+        /// Bookings are for 1 hour Spot is held from 9:00 to 9:59
+        /// </summary>
+        /// <param name="bookingTime"></param>
+        /// <returns></returns>
+        public async Task<bool> CheckIfSettlementsAreFullAsync(TimeOnly bookingTime)
+        {
+            var CurrentSettlements = await _context.Bookings
+                .Where(b => b.BookingTime.IsBetween(bookingTime, bookingTime.AddMinutes(59)))
+                .CountAsync();
+
+            return CurrentSettlements > 3 ? true : false;
+        }
+
     }
 }
