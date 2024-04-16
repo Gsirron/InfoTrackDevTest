663b1d0e2b5433a573febafe210e20d9f2a6b3f7
Norris Luong <33255034+Gsirron@users.noreply.github.com>
2024-04-16 18:28:30 +1000

    Feat: Removed Weatherforcast items and Added Booking Controller

---
+++ BookingApi/Controllers/BookingController.cs
@@ -0,0 +1,16 @@
+﻿using BookingApi.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingController : ControllerBase
+    {
+        public async Task<IActionResult> CreateBookingAsync([FromBody] BookingDto bookingDto)
+        {
+
+            return Ok();
+        }
+    }
+}
