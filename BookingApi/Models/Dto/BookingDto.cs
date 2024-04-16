aae77b610662a4ec6b58892ae38f1185444ac3a4
Norris Luong <33255034+Gsirron@users.noreply.github.com>
2024-04-16 18:29:26 +1000

    Feat: Added Validations to BookingDto

--- BookingApi/Models/Dto/BookingDto.cs
+++ BookingApi/Models/Dto/BookingDto.cs
@@ -1,8 +1,12 @@
-﻿namespace BookingApi.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingApi.Models.Dto
 {
     public class BookingDto
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Booking time is required")]
         public string BookingTime { get; set; }
     }
 }
