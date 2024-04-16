c50ac758612ac989f867be6a05ec8b52e3727393
Norris Luong <33255034+Gsirron@users.noreply.github.com>
2024-04-16 21:13:11 +1000

    Fix: Added regex annotation to ensure format is 24 hour time

--- BookingApi/Models/Dto/BookingDto.cs
+++ BookingApi/Models/Dto/BookingDto.cs
@@ -7,6 +7,8 @@ namespace BookingApi.Models.Dto
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Booking time is required")]
+        // Regular expresion to ensure format is HH:mm 24 hours time
+        [RegularExpression(@"^([01]?[0-9]|2[0-3]):[0-5][0-9]$", ErrorMessage = "Invalid time format, must be in the format HH:mm")]
         public string BookingTime { get; set; }
     }
 }
