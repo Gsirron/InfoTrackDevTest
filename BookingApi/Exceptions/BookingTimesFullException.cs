b3686f53240a32f23f0f53e3965db3ac82d8f11e
Norris Luong <33255034+Gsirron@users.noreply.github.com>
2024-04-16 18:41:06 +1000

    Feat: Added Custom Exceptions for the Controller

---
+++ BookingApi/Exceptions/BookingTimesFullException.cs
@@ -0,0 +1,11 @@
+﻿namespace BookingApi.Exceptions
+{
+    public class BookingTimesFullException : Exception
+    {
+        public BookingTimesFullException(string message) : base(message) { }
+
+        public BookingTimesFullException(string message, Exception innerException) : base(message, innerException){ }
+        public BookingTimesFullException(){ }
+
+    }
+}
---
+++ BookingApi/Exceptions/InvalidBookingDetailsException.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingApi.Exceptions
+{
+    public class InvalidBookingDetailsException:Exception
+    {
+        public InvalidBookingDetailsException() { }
+        public InvalidBookingDetailsException(string message) : base(message) { }
+        public InvalidBookingDetailsException(string message, Exception inner) : base(message, inner) { }
+
+    }
+}
